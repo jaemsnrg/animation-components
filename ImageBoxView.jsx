ad2ff96499ed8017228d8c6e976e798814abeede
twomoonscc <twomoonscc@gmail.com>
2026-05-15 13:05:47 +0200

    add parallax effect

--- ImageBoxView.jsx
+++ ImageBoxView.jsx
@@ -1,22 +1,35 @@
 'use client';
 
-import React from 'react';
-import { motion } from 'framer-motion';
+import React, { useRef } from 'react';
+import { motion, useScroll, useTransform } from 'framer-motion';
 import PropTypes from 'prop-types';
 
 // Expo-out easing for a dynamic, smooth reveal feel
 const EASE = [0.16, 1, 0.3, 1];
 
+// Extra image height factor to allow parallax movement without blank edges
+const PARALLAX_OVERFLOW = 1.3;
+
 export const ImageBoxView = ({
   src,
   alt,
   width = 400,
-  height = 520,
+  height = 640,
   initialScale = 1.3,
   borderRadius = 20,
+  parallaxAmount = 80,
 }) => {
+  const containerRef = useRef(null);
+
+  const { scrollYProgress } = useScroll({
+    target: containerRef,
+    offset: ['start end', 'end start'],
+  });
+
+  const imageY = useTransform(scrollYProgress, [0, 1], [-parallaxAmount / 2, parallaxAmount / 2]);
+
   return (
-    <div style={{ width }}>
+    <div ref={containerRef} style={{ width }}>
       <motion.div
         initial={{ height: 0 }}
         whileInView={{ height }}
@@ -37,10 +50,12 @@ export const ImageBoxView = ({
           transition={{ duration: 1.6, ease: EASE }}
           style={{
             width: '100%',
-            height,
+            height: height * PARALLAX_OVERFLOW,
             objectFit: 'cover',
             transformOrigin: 'center center',
             display: 'block',
+            y: imageY,
+            marginTop: -(height * (PARALLAX_OVERFLOW - 1)) / 2,
           }}
         />
       </motion.div>
@@ -55,4 +70,5 @@ ImageBoxView.propTypes = {
   height: PropTypes.number,
   initialScale: PropTypes.number,
   borderRadius: PropTypes.number,
+  parallaxAmount: PropTypes.number,
 };
